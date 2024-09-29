f64cebfa28b1a126aaa19896b1b3644e58fb0fa3
CodingAloha <jeremykalatorc@gmail.com>
2024-09-28 22:39:14 -1000

    Delete week5 directory

--- week5/linear_time.py
+++
@@ -1,5 +0,0 @@
-import timeit
-
-print(timeit.timeit("[x for x in range(1000000)]", number=1))
-print(timeit.timeit("[x for x in range(10000000)]", number=1))
-print(timeit.timeit("[x for x in range(100000000)]", number=1))
